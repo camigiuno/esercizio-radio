7d8749806ccd9ea326ba9ca5baad3828765f0fed
paolo <paolo@LAPTOP-PFR3VSID>
2024-01-24 18:44:48 +0100

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Radio
+    {
+        public double Volume { get; set; }
+        public Radio(double Volume)
+        {
+            this.Volume = Volume;
+        }
+        public double AumentaVolume()
+        {
+            return Volume + 0.10;
+        }
+        public double DiminuisciVolume()
+        {
+            return Volume - 0.10;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp17;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aumenta il volume");
+            double Volume = Convert.ToDouble(Console.ReadLine());
+            Radio radio = new Radio(Volume);
+            Console.WriteLine("{0}", radio.AumentaVolume());
+            Console.WriteLine("Diminuisci il volume");
+            Console.WriteLine("{0}", radio.DiminuisciVolume());
+        }
+    }
+}
